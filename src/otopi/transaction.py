cda0d26db2d6dc202c75476c163e8392c67e85c8
Alon Bar-Lev <alonbl@redhat.com>
2015-06-17 15:12:28 +0300

    core: transaction: destructor should be at unmanaged object
    The transaction element is a managed object, so there is no need for
    random destructor, also the abort is not re-entrant.
    
    The transaction is unmanaged one and abort is re-entrant, and may enjoy
    destructor usage for cleanup in case program misbehaves.
    
    Reported-By: Simone Tiraboschi <stirabos@redhat.com>
    Change-Id: I4dce1bb8d09e2bfda5cc6e76339378c33736d5a4
    Signed-off-by: Alon Bar-Lev <alonbl@redhat.com>

--- src/otopi/transaction.py
+++ src/otopi/transaction.py
@@ -40,10 +40,6 @@ class TransactionElement(base.Base):
         """Constructor."""
         super(TransactionElement, self).__init__()
 
-    def __del__(self):
-        """Destructor."""
-        self.abort()
-
     def __str__(self):
         """String representation."""
         return self.__name__
@@ -101,6 +97,10 @@ class Transaction(base.Base):
         self._prepared = []
         for element in elements:
             self.append(element)
+
+    def __del__(self):
+        """Destructor."""
+        self.abort()
 
     def __str__(self):
         return 'transaction'
