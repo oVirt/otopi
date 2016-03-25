27c4486e6fb005666a5d021b725adb1f8a39cca0
Sandro Bonazzola <sbonazzo@redhat.com>
2016-03-25 08:46:20 +0100

    automation: add functional testing
    Change-Id: I31b0c9f073bb412cd7961dcb2fa1006123083af1
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -2,3 +2,13 @@
 autoreconf -ivf
 ./configure --enable-java-sdk COMMONS_LOGGING_JAR=$(build-classpath commons-logging) JUNIT_JAR=$((build-classpath junit4 || build-classpath junit) | sed '/^$/d')
 make distcheck
+
+[[ -d tmp.repos ]] \
+|| mkdir -p tmp.repos
+yum-builddep otopi.spec
+rpmbuild \
+    -D "_topdir $PWD/tmp.repos" \
+    -ta otopi-*.tar.gz
+
+yum install -y $(find "$PWD/tmp.repos" -iname \*noarch\*.rpm)
+otopi
