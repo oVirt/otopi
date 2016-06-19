4a9402bcf8608917a5dfe974a0e9256d990f7f83
Yedidyah Bar David <didi@redhat.com>
2016-06-19 17:27:51 +0300

    automation: export built packages
    Change-Id: I130892f4ac6e6d827ab4e8cbb60c27888a6413cc
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>
    (cherry picked from commit cf9a6fe108bcae4b1b05cb877d7656532e84b66c)

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
+[[ -d exported-artifacts ]] \
+|| mkdir -p exported-artifacts
+
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+
 autoreconf -ivf
 ./configure --enable-java-sdk COMMONS_LOGGING_JAR=$(build-classpath commons-logging) JUNIT_JAR=$((build-classpath junit4 || build-classpath junit) | sed '/^$/d')
 make distcheck
@@ -8,7 +13,15 @@ make distcheck
 yum-builddep otopi.spec
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
     -ta otopi-*.tar.gz
 
-yum install -y $(find "$PWD/tmp.repos" -iname \*noarch\*.rpm)
+mv *.tar.gz exported-artifacts
+
+find \
+    "$PWD/tmp.repos" \
+    -iname \*.rpm \
+    -exec mv {} exported-artifacts/ \;
+
+yum install -y $(find "$PWD/exported-artifacts" -iname \*noarch\*.rpm)
 OTOPI_DEBUG=1 otopi
