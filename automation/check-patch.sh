4c0fac609dff4aeca0d628317c664ef9b077897e
Yedidyah Bar David <didi@redhat.com>
2016-09-15 16:03:04 +0300

    core: Use python3 on fedora >= 23
    Change-Id: If70f3cfda60d42a85f6ce8de1f4464ca940468e9
    Bug-Url: https://bugzilla.redhat.com/1316950
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,30 +1,9 @@
-#!/bin/bash -e
-[[ -d exported-artifacts ]] \
-|| mkdir -p exported-artifacts
+#!/bin/bash -xe
 
-SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
-
-autoreconf -ivf
-./configure --enable-java-sdk COMMONS_LOGGING_JAR=$(build-classpath commons-logging) JUNIT_JAR=$((build-classpath junit4 || build-classpath junit) | sed '/^$/d')
-make distcheck
-
-[[ -d tmp.repos ]] \
-|| mkdir -p tmp.repos
-yum-builddep otopi.spec
-rpmbuild \
-    -D "_topdir $PWD/tmp.repos" \
-    -D "release_suffix ${SUFFIX}" \
-    -ta otopi-*.tar.gz
-
-mv *.tar.gz exported-artifacts
-
-find \
-    "$PWD/tmp.repos" \
-    -iname \*.rpm \
-    -exec mv {} exported-artifacts/ \;
+. automation/common.sh
 
 yum install -y $(find "$PWD/exported-artifacts" -iname \*noarch\*.rpm)
 OTOPI_DEBUG=1 OTOPI_COVERAGE=1 COVERAGE_PROCESS_START="${PWD}/automation/coverage.rc" otopi
-coverage html -d exported-artifacts/coverage_html_report
+$COVERAGE html -d exported-artifacts/coverage_html_report
 cp automation/index.html exported-artifacts/
 
---
+++ automation/common.sh
@@ -0,0 +1,51 @@
+[[ -d exported-artifacts ]] \
+|| mkdir -p exported-artifacts
+
+# On fedora 23 and up, we want to use python3, as by default dnf is python3 and we want to be able to use it.
+distribution_name="$(python -c "import platform; print(platform.linux_distribution(full_distribution_name=0)[0])")"
+distribution_version="$(python -c "import platform; print(platform.linux_distribution(full_distribution_name=0)[1])")"
+if [ "${distribution_name}" == "fedora" -a "${distribution_version}" -ge 23 ]; then
+	DEVPKG=python3-devel
+	PEP8PKG=python3-pep8
+	FLAKGESPKG=python3-pyflakes
+	COVPKG=python3-coverage
+	PEP8=/usr/bin/python3-pep8
+	PYFLAKES=/usr/bin/python3-pyflakes
+	COVERAGE=/usr/bin/python3-coverage
+	PYTHON=/usr/bin/python3
+else
+	DEVPKG=python2-devel
+	PEP8PKG=python-pep8
+	FLAKGESPKG=pyflakes
+	COVPKG=python-coverage
+	PEP8=/usr/bin/pep8
+	PYFLAKES=/usr/bin/pyflakes
+	COVERAGE=/usr/bin/coverage
+	PYTHON=/usr/bin/python
+fi
+export PEP8 PYFLAKES PYTHON
+yum install -y $DEVPKG $PEP8PKG $FLAKGESPKG $COVPKG
+
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+
+autoreconf -ivf
+./configure --enable-java-sdk COMMONS_LOGGING_JAR=$(build-classpath commons-logging) JUNIT_JAR=$((build-classpath junit4 || build-classpath junit) | sed '/^$/d')
+make distcheck
+
+[[ -d tmp.repos ]] \
+|| mkdir -p tmp.repos
+yum-builddep otopi.spec
+rpmbuild \
+    -D "_topdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
+    -ta otopi-*.tar.gz
+
+mv *.tar.gz exported-artifacts
+
+find \
+    "$PWD/tmp.repos" \
+    -iname \*.rpm \
+    -exec mv {} exported-artifacts/ \;
+
+sed "s;@WORKDIR@;$PWD;" < automation/coverage.rc.in > automation/coverage.rc
+
