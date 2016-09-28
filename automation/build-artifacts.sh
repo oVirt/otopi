7499a545b4c14aee27375e3c145e84a2b7e87681
Yedidyah Bar David <didi@redhat.com>
2016-09-28 10:23:25 +0300

    Revert "core: Use python3 on fedora >= 23"
    This reverts commit 4c0fac609dff4aeca0d628317c664ef9b077897e.
    
    Reverting python3 as default, engine-setup is too broken.
    
    Change-Id: Iaadd3f6bd948dbcc993d98172d67964774eb120d
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -1,5 +1,25 @@
 #!/bin/bash -xe
+[[ -d exported-artifacts ]] \
+|| mkdir -p exported-artifacts
 
-. automation/common.sh
+[[ -d tmp.repos ]] \
+|| mkdir -p tmp.repos
+
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+
+autoreconf -ivf
+./configure
+make dist
+yum-builddep otopi.spec
+rpmbuild \
+    -D "_topdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
+    -ta otopi-*.tar.gz
+
+mv *.tar.gz exported-artifacts
+find \
+    "$PWD/tmp.repos" \
+    -iname \*.rpm \
+    -exec mv {} exported-artifacts/ \;
 
 createrepo exported-artifacts
