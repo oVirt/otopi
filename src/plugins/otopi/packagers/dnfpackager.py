80b3368a9d78ad174f237d4f6aabbca95f0e6c30
Rafael Martins <rmartins@redhat.com>
2017-02-13 13:17:56 +0100

    dnfpackager: do not print traceback when fail to import dnf
    Bug-Url: https://bugzilla.redhat.com/1404253
    Change-Id: I792d30e3a059e16a6899f1d8d3294e57f151a840
    Signed-off-by: Rafael Martins <rmartins@redhat.com>
    (cherry picked from commit 38257bf79f5b1ada2e847da68a49ef1dea2fa26f)

--- src/plugins/otopi/packagers/dnfpackager.py
+++ src/plugins/otopi/packagers/dnfpackager.py
@@ -160,7 +160,10 @@ class Plugin(plugin.PluginBase, packager.PackagerBase):
                         constants.PackEnv.YUMPACKAGER_ENABLED
                     ] = False
         except Exception:
-            self.logger.debug('Cannot initialize minidnf', exc_info=True)
+            # not calling with exc_info=True, because we always try to
+            # load DNF support first, polluting the logs with misleading
+            # tracebacks when running on yum-based operating systems
+            self.logger.debug('Cannot initialize minidnf')
 
     @plugin.event(
         before=(
