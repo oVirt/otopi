69cc05447e7509923a751dda99ffd61e95951cc4
Yedidyah Bar David <didi@redhat.com>
2017-09-12 16:31:53 +0300

    automation: Run otopi also with machine dialog
    To help catch failures such as the one fixed in:
    
    https://gerrit.ovirt.org/81659
    
    Change-Id: I17abd47b583cfac436fc546b605b7a98be579910
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -27,6 +27,7 @@ find \
 
 yum install -y $(find "$PWD/exported-artifacts" -iname \*noarch\*.rpm)
 otopi ODEBUG/packagesAction=str:install ODEBUG/packages=str:zziplib,zsh
+OTOPI_DEBUG=1 otopi DIALOG/dialect=str:machine
 OTOPI_DEBUG=1 OTOPI_COVERAGE=1 COVERAGE_PROCESS_START="${PWD}/automation/coverage.rc" otopi
 coverage html -d exported-artifacts/coverage_html_report
 cp automation/index.html exported-artifacts/
