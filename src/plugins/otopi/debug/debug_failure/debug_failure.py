521a711c7235c69f6b0158fd7f13567847db4c13
Yedidyah Bar David <didi@redhat.com>
2017-11-30 09:38:24 +0200

    plugins: debug: Fix debug_failure out of transaction
    Previous implementation was called only during MAIN_TRANSACTION, so did
    not catch failures before or after it, notably STAGE_CLOSEUP.
    
    New implementation uses otopi's notification mechanism, so should catch
    all error conditions. Also, it does not rely on an external 'ss' program
    to get tcp connection details.
    
    Change-Id: I0df8954c8a6e8715710161cf8d11dd7e49d79bd6
    Bug-Url: https://bugzilla.redhat.com/1518689
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>

--- src/plugins/otopi/debug/debug_failure/debug_failure.py
+++ src/plugins/otopi/debug/debug_failure/debug_failure.py
@@ -7,16 +7,33 @@
 
 
 import gettext
+import os
+import re
+import glob
 
 
-from otopi import constants
 from otopi import util
 from otopi import plugin
-from otopi import transaction
 
 
 def _(m):
     return gettext.dgettext(message=m, domain='otopi')
+
+
+PROC_NET_TCP = "/proc/net/tcp"
+SOCKET_STATES = {
+    '01': 'ESTABLISHED',
+    '02': 'SYN_SENT',
+    '03': 'SYN_RECV',
+    '04': 'FIN_WAIT1',
+    '05': 'FIN_WAIT2',
+    '06': 'TIME_WAIT',
+    '07': 'CLOSE',
+    '08': 'CLOSE_WAIT',
+    '09': 'LAST_ACK',
+    '0A': 'LISTEN',
+    '0B': 'CLOSING',
+}
 
 
 @util.export
@@ -24,52 +41,73 @@ class Plugin(plugin.PluginBase):
     """Debug Failure.
     """
 
-    class DebugFailureTransaction(transaction.TransactionElement):
-        """
-        Debug Failures transaction element
-        """
+    def _load_proc(self):
+        with open(PROC_NET_TCP, 'r') as f:
+            content = f.read().splitlines()
+            content.pop(0)  # Remove header
+        return content
 
-        def __init__(self, parent):
-            self._parent = parent
+    def _hex2dec(self, s):
+        return str(int(s, 16))
 
-        def __str__(self):
-            return _("Debug Failures Transaction")
+    def _hex_ip_to_decimal(self, s):
+        return '.'.join([self._hex2dec(s[i:i+2]) for i in (6, 4, 2, 0)])
 
-        def prepare(self):
+    def _hex_ip_port_to_str(self, s):
+        ip, port = s.split(':')
+        return ':'.join((self._hex_ip_to_decimal(ip), self._hex2dec(port)))
+
+    def _get_inode_pids(self):
+        res = {}
+        target_re = re.compile('socket:\[(\d*)\]')
+        try:
+            for pidpath in glob.glob('/proc/[0-9]*'):
+                pid = os.path.basename(pidpath)
+                try:
+                    for fdpath in glob.glob('%s/fd/[0-9]*' % pidpath):
+                        target = os.readlink(fdpath)
+                        match = target_re.match(target)
+                        if match:
+                            res[match.group(1)] = pid
+                except Exception:
+                    pass  # Skip if can't read, but continue with others
+        except Exception:
             pass
+        return res
 
-        def abort(self):
-            self._parent.execute(
-                args=(
-                    self._parent.command.get('ss'),
-                    '--all',
-                    '--numeric',
-                    '--processes',
-                ),
+    def _get_connections(self):
+        inodes_pids = self._get_inode_pids()
+        res = ['id uid local foreign state pid exe']
+        for line in self._load_proc():
+            line = line.split()
+            id = line[0]
+            local = self._hex_ip_port_to_str(line[1])
+            foreign = self._hex_ip_port_to_str(line[2])
+            state = SOCKET_STATES[line[3]]
+            uid = line[7]
+            pid = inodes_pids.get(line[9], 'UnknownPID')
+            try:
+                exe = os.readlink('/proc/%s/exe' % pid)
+            except Exception:
+                exe = 'UnknownEXE'
+            res.append(' '.join((id, uid, local, foreign, state, pid, exe)))
+        return res
+
+    def _notification(self, event):
+        if event == self.context.NOTIFY_ERROR:
+            self.logger.debug(
+                'tcp connections:\n%s',
+                '\n'.join(self._get_connections())
             )
 
     def __init__(self, context):
         super(Plugin, self).__init__(context=context)
 
     @plugin.event(
-        stage=plugin.Stages.STAGE_INIT,
-        after=(
-            constants.Stages.TRANSACTIONS_INIT,
-        ),
+        stage=plugin.Stages.STAGE_BOOT,
     )
     def _debug_failure_init(self):
-        # We add ourselves at STAGE_INIT, hopefully as first,
-        # because otopi (currently) calls the 'abort' methods in the same
-        # order that the elements were added, and we hope to be first, as
-        # close as possible to the failure.
-        self.environment[
-            constants.CoreEnv.MAIN_TRANSACTION
-        ].append(
-            self.DebugFailureTransaction(
-                parent=self,
-            )
-        )
-        self.command.detect('ss')
+        self.context.registerNotification(self._notification)
 
 
 # vim: expandtab tabstop=4 shiftwidth=4
