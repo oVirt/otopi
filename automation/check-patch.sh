d6dff7523fd964d56e61ef803a42e63f12940afc
Yedidyah Bar David <didi@redhat.com>
2018-03-08 15:21:41 +0200

    automation: Cleanups and fixes for fedora
    - Make build-artifacts-manual.sh a link to build-artifacts.sh , no need to
    have them different.
    
    - Remove check-merged.sh , we already check all we want in check-patch.sh .
    
    - Call build-artifacts.sh from check-patch.sh instead of duplicating.
    
    - Do not call createrepo ourselves - CI already does that.
    
    - use dnf-utils instead of yum-utils, 'dnf' instead of 'yum' etc.
    
    In addition to above, which was also done in master, do this for 1.7:
    
    - Test the packager only on el7, as it is broken on fedora
    
    Change-Id: I0ff5a2f3e3d688d81787bb385ed6ac85a18840d4
    Signed-off-by: Yedidyah Bar David <didi@redhat.com>
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,38 +1,26 @@
 #!/bin/bash -ex
-[[ -d exported-artifacts ]] \
-|| mkdir -p exported-artifacts
-
 autoreconf -ivf
 ./configure --enable-java-sdk COMMONS_LOGGING_JAR=$(build-classpath commons-logging) JUNIT_JAR=$((build-classpath junit4 || build-classpath junit) | sed '/^$/d')
 make distcheck
 
-SUFFIX=
-. automation/config.sh
-[ -n "${RELEASE_SUFFIX}" ] && SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+automation/build-artifacts.sh
 
-[[ -d tmp.repos ]] \
-|| mkdir -p tmp.repos
-yum-builddep otopi.spec
-rpmbuild \
-    -D "_topdir $PWD/tmp.repos" \
-    ${SUFFIX:+-D "release_suffix ${SUFFIX}"} \
-    -ta otopi-*.tar.gz
-
-mv *.tar.gz exported-artifacts
-
-find \
-    "$PWD/tmp.repos" \
-    -iname \*.rpm \
-    -exec mv {} exported-artifacts/ \;
-
-yum install -y $(find "$PWD/exported-artifacts" -iname \*noarch\*.rpm)
+DISTVER="$(rpm --eval "%dist"|cut -c2-3)"
+installer=""
+if [[ "${DISTVER}" == "el" ]]; then
+    installer=yum
+else
+    installer=dnf
+fi
+${installer} install -y $(find "$PWD/exported-artifacts" -iname \*noarch\*.rpm)
 
 cov_otopi() {
 	OTOPI_DEBUG=1 OTOPI_COVERAGE=1 COVERAGE_PROCESS_START="${PWD}/automation/coverage.rc" COVERAGE_FILE=$(mktemp -p $PWD .coverage.XXXXXX) otopi "$@"
 }
 
 # Test packager
-cov_otopi ODEBUG/packagesAction=str:install ODEBUG/packages=str:zziplib,zsh
+# In otopi 1.7, fedora/dnf is broken, do not run there for now.
+[[ "${DISTVER}" == "el" ]] && cov_otopi ODEBUG/packagesAction=str:install ODEBUG/packages=str:zziplib,zsh
 
 # Test command
 PATH="${PWD}/automation/testbin:$PATH" OTOPI_TEST_COMMAND=1 cov_otopi
